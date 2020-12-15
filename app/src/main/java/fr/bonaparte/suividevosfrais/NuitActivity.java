3a8c2d23d97dec94c1f232ab1335c307c39f1092
Coline ALBERTI <coline.alberti@gmail.com>
2020-12-15 15:15:17 +0100

    Nuitee

--- app/src/main/java/fr/bonaparte/suividevosfrais/NuitActivity.java
+++ app/src/main/java/fr/bonaparte/suividevosfrais/NuitActivity.java
@@ -1,13 +1,17 @@
 package fr.bonaparte.suividevosfrais;
 
 import android.os.Bundle;
+import android.widget.DatePicker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class NuitActivity extends AppCompatActivity {
     @Override
-    protected void onCreate(Bundle aaa) {
-        super.onCreate(aaa);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nuit);
+        setTitle("GSB : Frais Nuitées");
+        // modification de l'affichage du DatePicker; format date
+        Global.changeAfficheDate((DatePicker) findViewById(R.id.datNuitee), false);
     }
 }
