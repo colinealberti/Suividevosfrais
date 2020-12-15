d8406b1ac5d4f238326f17d8bdfe2ce75fe64d9e
Coline ALBERTI <coline.alberti@gmail.com>
2020-12-15 14:09:37 +0100

    Initial commit

---
+++ app/src/main/java/fr/bonaparte/suividevosfrais/NuitActivity.java
@@ -0,0 +1,13 @@
+package fr.bonaparte.suividevosfrais;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NuitActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle aaa) {
+        super.onCreate(aaa);
+        setContentView(R.layout.activity_nuit);
+    }
+}
---
+++ app/src/main/java/fr/bonaparte/suividevosfrais/test.java
@@ -0,0 +1,14 @@
+package fr.bonaparte.suividevosfrais;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class test extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle aaa) {
+        super.onCreate(aaa);
+        setContentView(R.layout.activity_nuit);
+        setTitle("GSB : Frais Km");
+}}
